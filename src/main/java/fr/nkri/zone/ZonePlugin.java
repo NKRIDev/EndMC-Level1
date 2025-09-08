5a9105458c767116607dcce4a46ad779a49968ab
NKRI <nkri.dev@gmail.com>
2025-09-08 21:28:58 +0200

    feat(manager) : create manager class

--- src/main/java/fr/nkri/zone/ZonePlugin.java
+++ src/main/java/fr/nkri/zone/ZonePlugin.java
@@ -1,5 +1,6 @@
 package fr.nkri.zone;
 
+import fr.nkri.zone.managers.ZoneManager;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,9 +12,17 @@ public class ZonePlugin extends JavaPlugin {
     @Getter
     private static ZonePlugin INSTANCE;
 
+    /**
+     * Init managers
+     */
+    private ZoneManager zoneManager;
+
     @Override
     public void onEnable() {
         INSTANCE = this;
+
+        //init managers
+        this.zoneManager = new ZoneManager();
     }
 
     @Override
---
+++ src/main/java/fr/nkri/zone/managers/ZoneManager.java
@@ -0,0 +1,13 @@
+package fr.nkri.zone.managers;
+
+public class ZoneManager {
+
+    /**
+     * Instance of area manager
+     */
+    private static ZoneManager INSTANCE;
+
+    public ZoneManager(){
+        INSTANCE = this;
+    }
+}
